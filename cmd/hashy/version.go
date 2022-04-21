9bb651f5c3df2c434c2657a99be27d8a482fc773
Scott Leggett <scott@sl.id.au>
2022-04-21 13:59:39 +0800

    feat: implement the version command

--- cmd/hashy/version.go
+++ cmd/hashy/version.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
 // VersionCmd represents the version command.
 type VersionCmd struct{}
+
+// Run the version command to print version information.
+func (cmd *VersionCmd) Run() error {
+	fmt.Printf("hashy %v (%v) compiled with %v on %v\n", version,
+		shortCommit, goVersion, date)
+	return nil
+}
